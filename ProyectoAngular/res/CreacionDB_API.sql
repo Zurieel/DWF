d2880427900b6737d54772b334de4031c48d47c0
Zurieel <zurielmtz1908@ciencias.unam.mx>
2024-02-23 11:40:02 -0600

    FIX: Errores en archivo sql

--- ProyectoAngular/res/CreacionDB_API.sql
+++ ProyectoAngular/res/CreacionDB_API.sql
@@ -86,7 +86,7 @@ DROP TABLE IF EXISTS item;
 CREATE TABLE item(
 	item_id INT NOT NULL AUTO_INCREMENT,
     invoice_id INT NOT NULL,
-    gtin CHAR(13) UNIQUE NOT NULL,
+    gtin CHAR(13) NOT NULL,
     quantity INT NOT NULL,
 	unit_price FLOAT NOT NULL,
 	subtotal FLOAT NOT NULL,
